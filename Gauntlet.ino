b336f0d69275613fc34dd13e80cfd6fe5d6df63d
Joflixen <Joflixen@users.noreply.github.com>
2017-04-01 17:24:38 +1100

    Gauntlet
    Gauntlet (glove) controller, using a Arduino Nano clone.
    
    ## Gloves (Gauntlet Electronics)
    The attached schematic is a basic wiring diagram for the Arduino Nano v3 clone board used to drive the Gloves.
    Electronics is fitted into the Forearm (Gauntlet) of the suit, and connected to to glove via plug/socket.
    
    ## Intended Feature Operation
    - Controlled by one micro-switch in the back of the wrist, activated by tilting the hand back (as if to fire repulsor weapon)
    - On action, LED Array powers up in brightness along with ascending tone output to pezo speaker
    - At top of defined "power up" trigger atmosphere release from sprayer system (currently indicated by on-board LED)
    - If the full-"power up" is not reached (ie decided to not-shoot); power down is performed causing a descending tone and LED brightness
    
    ## TO DO
    Finer points of atmosphere release is not defined at this time; as nano-mister floods space with too much water.
    Alternatives being sought which have less residue.

---
+++ Gauntlet.ino
@@ -0,0 +1,92 @@
+/*
+*/
+// constants //
+const int buttonPin = 4;          // PIN for switch to ground
+const int tonePin = 9;            // PIN for Speaker to ground through 100ohm resistor
+const int ledPin =  5;            // PIN for LED OUTPUT
+const int ledBoard =  13;         // Onboard LED (indicates top of ramp)
+const int delaySpeed = 4;         // Time between next step of the change       
+const int sprayDuration = 2000;    // Intervals the spray-jet is on for (2sec ~= 400)
+const int pwmIntervals = 300;     // Howmany times it changes brightness/sound frequency
+const int toneFreqLow   = 3250;   // Tone Freqency at bottom of Ramp-up
+const int toneFreqHigh  = 5500;   // Tone Frequency at top of Ramp-Up
+
+//const int pwmIntervals = 2000;     // Howmany times it changes brightness/sound frequency
+//const int toneFreqLow   = 5000;   // Tone Freqency at bottom of Ramp-up
+//const int toneFreqHigh  = 25000;   // Tone Frequency at top of Ramp-Up
+
+
+// --------------------------------------------------------------------------------------//
+// variables //
+int buttonState = 0;            // variable for reading the pushbutton status
+int fadeValue = 0;              // variable for storing the PWM duty
+int pwmInt = 0;                 // variable for which interval we are at 
+int toneValue = toneFreqLow;    // Preset the starting tone value to lower ramp frequency
+int sprayDuty = sprayDuration;  // Remainder of Spray Intervals
+float Rled;                     // Log curve calulation for LED
+int rampDown = 0;
+
+// --------------------------------------------------------------------------------------//
+// Initialise stuff //
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(tonePin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Rled = (pwmIntervals * log10(2))/(log10(255));
+}
+
+// Run the Stuff //
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    // FADE UP //
+    if (pwmInt < pwmIntervals){
+      pwmInt+=1.5;
+      fadeValue = pow(2, (pwmInt / Rled)) -1;
+      setTone(pwmInt);
+      rampDown = 1;
+    } else {
+      // FIRE //
+      rampDown = 0;
+      noTone(tonePin);    
+     
+      // Handle the Sprayer
+      if (sprayDuty > 1){
+        digitalWrite(ledBoard, HIGH);
+        sprayDuty--;
+//        tone(10, 2500);
+//        delay(delaySpeed);
+//        tone(10, 3000);
+      } else {
+        // FIRE //
+        digitalWrite(ledBoard, LOW);
+        //noTone(10);    
+      }
+    }
+  } else {
+    // FADE OUT //
+    digitalWrite(ledBoard, LOW);
+    sprayDuty = sprayDuration;
+    
+    if (pwmInt > 0 ){
+      pwmInt--;
+//      setTone(pwmInt);
+      fadeValue = pow(2, (pwmInt / Rled)) -1;
+      if (rampDown > 0){
+          setTone(pwmInt);
+      }
+    } else {
+      noTone(tonePin);
+    }
+  }
+  analogWrite(ledPin, fadeValue);
+  delay(delaySpeed);
+}
+
+// Helper function sets the tone frequency based on interval-step
+void setTone(int interval){
+  int freqRange = (toneFreqHigh-toneFreqLow);
+  toneValue = toneFreqLow + ( (freqRange/pwmIntervals) * interval );
+  tone(tonePin, toneValue);
+}
+
